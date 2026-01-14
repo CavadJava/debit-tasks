d253dba430fc9999ece9363a708f7515d36215dd
cavadjava <cavadjava@gmail.com>
2026-01-14 20:51:46 +0400

    added NotFound, login signup

--- routerwebpages/src/App.jsx
+++ routerwebpages/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Suspense,lazy } from 'react'
 import { getProfile } from './services/auth.js';
 import { useFetchData } from './hooks/useFetchData.js';
+import Login from './pages/auth/login.jsx';
+import SignUp from './pages/auth/signup.jsx';
+import NotFound from './pages/not_found.jsx';
 
 const Home = lazy(() => import('./pages/home.jsx'));
 const About = lazy(() => import('./pages/about/about.jsx'));
@@ -20,12 +23,25 @@ function App() {
       <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
+
+        { !data ? (
+          <>
+            <Route path="/auth/login" element={<Login />} />
+            <Route path="/auth/signup" element={<SignUp />} />
+          </>
+         ) :
+        <>
+
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/menu/:menuId" element={<ProductDetail />}  />
+
+        </>
+      }
+          <Route path="/*" element={<NotFound/>} />
         </Routes>
       </Suspense>
       </BrowserRouter>
---
+++ routerwebpages/src/pages/auth/signup.jsx
@@ -0,0 +1,10 @@
+
+export default function SignUp () {
+    return (
+        <>
+            <div>
+                <h2>SignUp Page</h2>
+            </div>
+        </>
+    )
+}
