7efc2e4a6dedad3433b8d9821cfffcc9546b7093
cavadjava <cavadjava@gmail.com>
2026-01-14 21:42:30 +0400

    added button logics

--- routerwebpages/src/pages/about/about.jsx
+++ routerwebpages/src/pages/about/about.jsx
@@ -1,13 +1,23 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 import Headers from '../../components/header.jsx'
 import Footer from '../../components/footer.jsx'
 import './about.css'
 
 export default function About() {
+    const [activeTab, setActiveTab] = useState();
+
+    const handleAboutCompany = () => {
+        setActiveTab("company");
+    }
+    const handleAboutProducts = () => {
+        setActiveTab("product");
+    }
+
+
     return (
         <>
-        <Headers/>
-            <div className="about-page">
+            <Headers/>
+            <div className="about-page" style={{ textAlign: "center" }}>
                 <h1>Learn About Us</h1>
                 <img 
                     src="https://router-web-topic-7.vercel.app/assets/port-8b357f33.jpg" 
@@ -18,14 +28,27 @@ export default function About() {
                         marginBottom: "24px" 
                     }} 
                 />
-                {/* <Link to="/about/company">About Company</Link> */}
-                {/* <Link to="/about/products">About Products</Link> */}
-                {/* <Routes>
-                    <Route path="/about/company" element={<h2>About Company</h2>} />
-                    <Route path="/about2/products" element={<h2>About Products</h2>} />
-                </Routes> */}
+                
+                <div className="button-group">
+                    <button style={{
+                        border: 'none',
+                        cursor: 'pointer',
+                        fontWeight: 'bold',
+                    }}
+     onClick={handleAboutCompany}>About Company</button>
+                    <button onClick={handleAboutProducts}>About Products</button>
+                </div>
+                
+                {
+                    activeTab === 'company' ?
+                    (<h1>About Company</h1> )
+                        : activeTab === 'product' ?
+                    (<h1>About Product</h1>)
+                        : (<p></p>)
+                }
+                
             </div>
-        <Footer/>
+            <Footer/>
         </>
     )
 }
